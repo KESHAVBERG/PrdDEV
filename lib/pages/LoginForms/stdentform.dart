314c34c254f1bfbef76ad5d373125bd20542457d
KESHAVBERG <kingkeshav7@gmail.com>
2021-12-18 09:44:18 +0530

    done with login ui

---
+++ lib/pages/LoginForms/stdentform.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class StudentForm extends StatefulWidget {
+  const StudentForm({Key? key}) : super(key: key);
+
+  @override
+  _StudentFormState createState() => _StudentFormState();
+}
+
+class _StudentFormState extends State<StudentForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text("Student Login"),
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: h,
+          width: w,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  validator: (val){
+                    if(val!.isEmpty){
+                      return "enter your email";
+                    }else if(val.contains("@")){
+                      return "enter a vail Email";
+                    }else{
+                      return null;
+                    }
+                  },
+                  decoration:const InputDecoration(
+                    hintText:"email",
+                    hintStyle: TextStyle(
+                      color: Colors.white
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                  ),
+                ),
+                TextFormField(
+                  controller: passwordController,
+
+                  validator: (val){
+                    if(val!.isEmpty){
+                      return "enter your password";
+                    }else if(val.length<6){
+                      return "more than 6 chars";
+                    }else{
+                      return null;
+                    }
+                  },
+                  decoration:const InputDecoration(
+                    hintText:"password",
+                    hintStyle: TextStyle(
+                        color: Colors.white
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                GestureDetector(
+                  onTap: (){
+                    if(_formKey.currentState!.validate()){
+
+                    }
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    height: 50,
+                    width: w,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius:BorderRadius.circular(50),
+                    ),
+                    child:const Text("Login",style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white
+                    ),),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,5 @@
+import 'package:classadmin/pages/LoginForms/stdentform.dart';
+import 'package:classadmin/pages/LoginForms/teacherlogin.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -10,6 +12,62 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
+    return Scaffold(
+
+      body: Container(
+        color: Colors.grey[900],
+        height: h,
+        width: w,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const StudentForm()));
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 30),
+                  height: 50,
+                  width: w,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius:BorderRadius.circular(50),
+                  ),
+                  child:const Text("Student Login",style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                  ),),
+                ),
+              ),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>const TeacherLogin()));
+
+              },
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                height: 50,
+                width: w,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius:BorderRadius.circular(50),
+                ),
+                child:const Text("Teacher Login",style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                ),),
+              ),
+            ),          ],
+        ),
+      ),
+    );
   }
 }
